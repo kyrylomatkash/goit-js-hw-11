0b6b5438c868c43b2a78f9e9b863b15f788fd03c
Kyrylo <kyrylo.mm@gmail.com>
2023-11-06 04:24:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,3 +38,52 @@ async function searchImages(query) {
       throw error;
     }
   }
+  // Завантаження зображень
+function renderImages(imageData) {
+    if (page === 1) {
+      gallery.innerHTML = '';
+    }
+    imageData.hits.forEach((image) => {
+      const photoLink = document.createElement('a');
+      photoLink.href = image.largeImageURL;
+      photoLink.classList.add('photo-card');
+  
+      const img = document.createElement('img');
+      img.src = image.webformatURL;
+      img.alt = image.tags;
+      img.loading = 'lazy';
+  // Змінні для інформації про зображення
+      const likes = image.likes;
+      const comments = image.comments;
+      const downloads = image.downloads;
+  // Створення елементів для відображення інформації про зображення
+      const detailsContainer = document.createElement('div');
+      detailsContainer.classList.add('image-details');
+  
+      const likesElement = document.createElement('p');
+      likesElement.textContent = `Likes: ${likes}`;
+  
+      const commentsElement = document.createElement('p');
+      commentsElement.textContent = `Comments: ${comments}`;
+  
+      const downloadsElement = document.createElement('p');
+      downloadsElement.textContent = `Downloads: ${downloads}`;
+  
+      detailsContainer.appendChild(likesElement);
+      detailsContainer.appendChild(commentsElement);
+      detailsContainer.appendChild(downloadsElement);
+  
+      photoLink.appendChild(img);
+      photoLink.appendChild(detailsContainer);
+      gallery.appendChild(photoLink);
+    });
+ // Показ кнопки Load more для підвантаження нових зображень
+    if (imageData.totalHits > page * perPage) {
+      showLoadMoreButton();
+    } else {
+      hideLoadMoreButton();
+    }
+  
+    initializeSimpleLightbox();
+    smoothScrollToGallery();
+  }
